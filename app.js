02972d649ba6ab21595c0b0e548e800ced359fe9
JayantaRana <113259493+JayantaRana@users.noreply.github.com>
2024-08-18 12:14:38 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
+const cors = require('cors'); // Import CORS
 
 require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 3000;
+// Access-Control-Allow-Origin: '*';
+
+app.use(cors({
+    origin: 'https://appdownloader-three.vercel.app', // Replace this with the exact URL of your frontend
+    methods: 'GET,POST',
+    allowedHeaders: 'Content-Type'
+}));
+// app.use(cors());
+
+
+
 
 mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
@@ -28,15 +39,19 @@ app.use(express.json());
 
 app.post('/api/download', async (req, res) => {
     try {
+        console.log('Received download request');
         let download = await Download.findOne();
+        console.log('Download record:', download);
         if (!download) {
             download = new Download();
         }
         download.count += 1;
         await download.save();
+        console.log('Download count updated',download);
         res.status(200).json({ message: 'Download recorded', count: download.count });
     } catch (err) {
-        res.status(500).json({ error: 'Internal Server Error' });
+        console.error('Error during download recording:', err);
+        res.status(500).json({ error: 'Internal Server Error3',err });
     }
 });
 
@@ -45,7 +60,7 @@ app.get('/api/download', async (req, res) => {
         const download = await Download.findOne();
         res.status(200).json({ count: download ? download.count : 0 });
     } catch (err) {
-        res.status(500).json({ error: 'Internal Server Error' });
+        res.status(500).json({ error: 'Internal Server Error2',err });
     }
 });
 
